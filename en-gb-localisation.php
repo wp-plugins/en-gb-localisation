a4060cb8dc006231c0fa0916bc1b7a2b4ff85679
Kawauso <Kawauso@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-26 09:53:41 +0000

    git-svn-id: https://plugins.svn.wordpress.org/en-gb-localisation/trunk@389303 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ en-gb-localisation.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Plugin Name: en_GB localisation
+Description: Fixes those little things in WordPress like misplaced vowels and over-use of the letter Z.
+Author: Adam Harley
+Version: 1.0.1
+*/
+
+function gb_gettext( $text ) {
+	static $replace, $keys;
+
+	if ( !is_array($replace) ) {
+		$replace = array(
+			'You are about to install WordPress %s <strong>in English (US).</strong> There is a chance this update will break your translation. You may prefer to wait for the localized version to be released.'
+				=> 'You are about to install WordPress %s <strong>in English (US).</strong> There is a chance this may expose you briefly to Americanisms. Stay calm and carry on.',
+
+			'Trash' => 'Bin',
+
+			'Color' => 'Colour',
+			'color' => 'colour',
+			'favorite' => 'favourite',
+			'neighbor' => 'neighbour',
+
+			// Cambridge > Oxford
+			'authorize' => 'authorise',
+			'categorize' => 'categorise',
+			'customize' => 'customise',
+			'localized' => 'localised',
+			'minimize' => 'minimise',
+			'optimize' => 'optimise',
+			'Optimize' => 'Optimise',
+			'Organize' => 'Organise',
+			'recognize' => 'recognise',
+			'utilize' => 'utilise',
+		);
+
+		$hour = date('G', time() + (get_option('gmt_offset')/* in hrs*/ * 60 * 60) );
+
+		if ( $hour <= 4 )
+			$replace['Howdy, %1$s'] = 'Go to bed, %1$s';
+		else if ( $hour < 12 )
+			$replace['Howdy, %1$s'] = 'Mornin\', %1$s';
+		else if ( $hour <= 16 )
+			$replace['Howdy, %1$s'] = 'Afternoon, %1$s';
+		else
+			$replace['Howdy, %1$s'] = 'Evenin\', %1$s';
+
+		$keys = array_keys( $replace );
+	}
+
+	return str_replace( $keys, $replace, $text );
+}
+
+add_filter( 'gettext', 'gb_gettext' );
+add_filter( 'ngettext', 'gb_gettext' );
+add_filter( 'ngettext_with_context', 'gb_gettext' );
---
+++ readme.txt
@@ -0,0 +1,45 @@
+=== en_GB (British English) Translation ===
+Contributors: Kawauso
+Tags: en_GB, translation, localisation
+Requires at least: 3.0
+Tested up to: 3.2
+Stable tag: 1.0.1
+
+Fixes those little things in WordPress like misplaced vowels and over-use of the letter Z.
+
+== Description ==
+
+<strong>Changes:</strong>
+
+* Trash -> Bin
+* Color -> Colour (and similar)
+* Categorize -> Categorise (and similar)
+* Howdy -> greeting based on the time of day (in 3.2 and higher)
+
+== Installation ==
+
+1. Upload `en-gb-localisation.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Optionally set your locale (`WPLANG`) to en_GB in `wp-config.php`
+
+== Frequently Asked Questions ==
+
+= Are you serious? =
+Reasonably so. Seeing 'Trash' too often sent me over the edge.
+
+= Why isn't this a mo/po file? =
+Since en_GB is quite similar to en_US, it would be rather unnecessary to use full string translations that may require updating for each version.
+
+= Using -ize isn't an Americanism =
+While I'm aware of this, I loathe [Oxford spelling](http://en.wikipedia.org/wiki/American_and_British_English_spelling_differences#-ise.2C_-ize_.28-isation.2C_-ization.29) with a passion.
+
+== Changelog ==
+
+= 1.0.1 =
+* First public release
+* Made strings static (thanks dd32)
+
+= 1.0 =
+* First version
+
+== Upgrade Notice ==
